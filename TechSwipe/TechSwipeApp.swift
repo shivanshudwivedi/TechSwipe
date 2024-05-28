1532d58e14b6670cffa587b548a851dced8b6a63
Shivanshu <shivanshu.dwivedi@trincoll.edu>
2024-05-28 12:20:30 -0400

    Initial Commit

---
+++ TechSwipe/TechSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechSwipeApp.swift
+//  TechSwipe
+//
+//  Created by Shivanshu Dwivedi on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TechSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
